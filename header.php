e21035e63433e78f5fd80978eac459eadd4aacd9
khmuhib <khmuhib2013@gmail.com>
2023-10-05 12:07:13 +0600

    file convert to html to php

---
+++ header.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+
+<html dir="ltr" lang="EN-GB" xml:lang="EN-GB">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta name="geo.region" content="GB" />
+    <meta name="geo.position" content="51.524501;-0.07152" />
+    <meta name="ICBM" content="51.524501, -0.07152" />
+
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Terms &amp; Conditions</title>
+
+    <meta property="og:title" content="Terms &amp; Conditions">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://www.chefonline.com/epos/terms-and-conditions">
+    <meta property="og:image" content="http://www.YOURWEBSITEHERE.com/og-image.jpg">
+    <meta property="og:image:width" content="300">
+    <meta property="og:image:height" content="300">
+    <meta property="og:site_name" content="ChefOnline EPoS">
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@SRS_ChefOnline">
+    <meta name="twitter:title" content="Terms &amp; Conditions">
+    <meta name="twitter:image" content="http://www.YOURWEBSITEHERE.com/og-image.jpg">
+    <meta name="twitter:image:alt" content="ChefOnline EPoS">
+
+    <script src="javascript/jquery/jquery-2.1.1.min.js" type="text/javascript"></script>
+    <script src="javascript/bootstrap/js/bootstrap.min.js" type="text/javascript"></script>
+
+    <link href="javascript/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="stylesheet.css" rel="stylesheet">
+
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/carousel.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/custom.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/lightbox.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/animate.css" />
+    <link rel="stylesheet" type="text/css" href="stylesheet/megnor/search_suggestion.css" />
+    <link rel="stylesheet" type="text/css" href="javascript/jquery/magnific/magnific-popup.css" />
+
+    <link href="javascript/search_suggestion/jquery-ui.css" type="text/css" rel="stylesheet" media="screen" />
+
+    <script type="text/javascript" src="javascript/megnor/custom.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jstree.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/carousel.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/megnor.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery.custom.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery.formalize.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery.elevatezoom.min.js"></script>
+    <script type="text/javascript" src="javascript/lightbox/lightbox-2.6.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/tabs.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery.bxslider.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/bootstrap-notify.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery-migrate-1.2.1.min.js"></script>
+    <script type="text/javascript" src="javascript/megnor/jquery.easing.1.3.js"></script>
+    <script type="text/javascript" src="javascript/megnor/doubletaptogo.js"></script>
+    <script type="text/javascript" src="javascript/megnor/parallex.js"></script>
+    <script type="text/javascript" src="javascript/jquery/magnific/jquery.magnific-popup.min.js"></script>
+
+    <script src="javascript/common.js" type="text/javascript"></script>
+    <link href="https://chefonline.com/epos/image/catalog/favicon.png" rel="icon" />
+
+    <script src="javascript/search_suggestion/search_suggestion.js" type="text/javascript"></script>
+    <script src="javascript/search_suggestion/jquery-ui.js" type="text/javascript"></script>
+
+</head>
+
+
+<body class="information-information-5 layout-1">
+
+    <header id="header">
+        <div class="header-container">
+            <div class="row">
+                <div class="col-md-2 col-sm-2 header-logo">
+                    <div id="logo">
+                        <a href="index.html">
+                            <img src="image/catalog/logo-black.png" title="ChefOnline EPoS" alt="ChefOnline EPoS" class="img-responsive" />
+                        </a>
+                    </div>
+                </div>
+
+                <nav class="col-md-8 col-sm-8 nav-container" role="navigation">
+                    <div class="nav-inner">
+                        <div class="container">
+                            <div id="menu" class="main-menu">
+                                <ul class="nav navbar-nav">
+                                    <li class="top_level"> <a href="index.php">Home</a></li>
+                                    <li class="top_level"><a href="package.php">ChefOnline EPoS Packages</a></li>
+                                    <li class="top_level"><a href="addon.php">Add ons</a></li>
+                                    <li class="top_level"><a href="about.php">About Us</a></li>
+                                    <li class="top_level"><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+
+                            <div id="res-menu" class="main-menu nav-container1">
+                                <ul class="main-navigation">
+                                    <li><a href="package.php">ChefOnline EPoS Packages</a></li>
+                                    <li><a href="addon.php">Add ons</a></li>
+                                    <li><a href="about.php">About Us</a></li>
+                                    <li><a href="terms_and_condition.php">Terms and Conditions</a></li>
+                                    <li><a href="contact.php">Contact</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </nav>
+                <div class="header-search">
+                    <div class="search">
+                        <div id="search" class="input-group">
+                            <div onclick="focusMethod()" class="search_button">Search</div>
+                            <div class="searchbox">
+                                <div class="searchinner">
+                                    <input id="focus-input" type="text" name="search" value="" placeholder="Search" class="form-control input-lg search-autocomplete" autofocus />
+                                    <button type="button" class="btn btn-default btn-lg"></button>
+                                </div>
+                            </div>
+                        </div>
+
+                        <script type="text/javascript">
+                            $('input[name=search]').focus();
+                            focusMethod = function getFocus() {
+                                $('input[name=search]').focus();
+                            }
+                            /* Search */
+                            $('#search input[name=\'search\']').parent().find('button').on('click', function() {
+                                url = $('base').attr('href') + 'index.php?route=product/search';
+
+                                var search = $('.header-search input[name=\'search\']').val();
+
+                                if (search) {
+                                    url += '&search=' + encodeURIComponent(search);
+                                }
+
+                                var search = $('.header-search input[name=\'search\']').prop('value');
+
+                                var category_id = $('.header-search select[name=\'category_id\']').prop('value');
+
+                                if (category_id > 0) {
+                                    url += '&category_id=' + encodeURIComponent(category_id);
+                                }
+
+                                location = url;
+                            });
+
+                            $('#search input[name=\'search\']').on('keydown', function(e) {
+                                if (e.keyCode == 13) {
+                                    $('.header-search input[name=\'search\']').parent().find('button').trigger('click');
+                                }
+                            });
+
+                            $('select[name=\'category_id\']').on('change', function() {
+                                if (this.value == '0') {
+                                    $('input[name=\'sub_category\']').prop('disabled', true);
+                                } else {
+                                    $('input[name=\'sub_category\']').prop('disabled', false);
+                                }
+                            });
+
+                            $('select[name=\'category_id\']').trigger('change');
+                        </script>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- ======= Quick view JS ========= -->
+    <script>
+        function quickbox() {
+            if ($(window).width() > 767) {
+                $('.quickview-button').magnificPopup({
+                    type: 'iframe',
+                    delegate: 'a',
+                    preloader: true,
+                    tLoading: 'Loading image #%curr%...',
+                });
+            }
+        }
+        jQuery(document).ready(function() {
+            quickbox();
+        });
+        jQuery(window).resize(function() {
+            quickbox();
+        });
+    </script>
